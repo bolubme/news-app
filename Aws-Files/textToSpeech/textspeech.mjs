b77a16cad62d209471f0206e14d4c4b9d9bd58c8
bolu <morolaribolu@gmail.com>
2025-09-26 00:51:28 +0100

    Updates

--- Aws-Files/textToSpeech/textspeech.mjs
+++ Aws-Files/textToSpeech/textspeech.mjs
@@ -1,16 +1,15 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({ apiKey: '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' });
+const openai = new OpenAI({ apiKey: "" });
 
-
-export async function convertTextToSpeech(text){
+export async function convertTextToSpeech(text) {
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
-    input: text
-  })
-  
-  const audioBuffer = Buffer.from(await mp3.arrayBuffer())
-  
+    input: text,
+  });
+
+  const audioBuffer = Buffer.from(await mp3.arrayBuffer());
+
   return audioBuffer;
 }
